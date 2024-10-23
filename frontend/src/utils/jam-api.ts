e3fef4629a3bd837880401835128376f5772b2fa
Ryan Rivera <37811096+ryrr@users.noreply.github.com>
2024-10-22 22:32:18 -0400

    add api endpoints
    -add moveCompanies
    -add moveAllCompanies
    -add getJobStatusListener
    - exposes endpoints to the FE for movement of companies across collections

--- frontend/src/utils/jam-api.ts
+++ frontend/src/utils/jam-api.ts
@@ -17,6 +17,11 @@ export interface ICompanyBatchResponse {
     companies: ICompany[];
 }
 
+export interface IMoveCompaniesResponse{
+    job_id:string
+    message:string
+}
+
 const BASE_URL = 'http://localhost:8000';
 
 export async function getCompanies(offset?: number, limit?: number): Promise<ICompanyBatchResponse> {
@@ -55,6 +60,45 @@ export async function getCollectionsMetadata(): Promise<ICollection[]> {
         return response.data;
     } catch (error) {
         console.error('Error fetching companies:', error);
+        throw error;
+    }
+}
+
+export async function moveCompanies(ids : number[], sourceCollection : string, targetCollection : string): Promise<IMoveCompaniesResponse> {
+    const payload = {
+        company_ids:ids,
+        source_collection:sourceCollection,
+        target_collection:targetCollection
+    }
+    try {
+        const response = await axios.post(`${BASE_URL}/companies/moveMultiple`,payload);
+        return response.data;
+    } catch (error) {
+        console.error('Error moving companies:', error);
+        throw error;
+    }
+}
+
+export async function moveAllCompanies(sourceCollection : string, targetCollection : string): Promise<IMoveCompaniesResponse> {
+    const payload = {
+        source_collection:sourceCollection,
+        target_collection:targetCollection
+    }
+    try {
+        const response = await axios.post(`${BASE_URL}/companies/moveAll`,payload);
+        return response.data;
+    } catch (error) {
+        console.error('Error moving companies:', error);
+        throw error;
+    }
+}
+
+export async function getJobStatusListener(jobId : string): Promise<any> {
+    try {
+        const response = await axios.get(`${BASE_URL}/companies/job-status/${jobId}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching job:', error);
         throw error;
     }
 }
