9d2193882c61eae53f3d049c1fc7855c808f82a1
Ryan Rivera <37811096+ryrr@users.noreply.github.com>
2024-10-22 22:03:44 -0400

    backend update
    - remove moveCompany function
    - add moveMultiple route/FN
    - add get_job_status route/FN
    - add I/O classes
    -add helper functions that will be moved later

--- backend/backend/routes/companies.py
+++ backend/backend/routes/companies.py
@@ -1,8 +1,12 @@
 import uuid
 import math
 from fastapi import APIRouter, Depends, Query, HTTPException, BackgroundTasks
+from fastapi.responses import StreamingResponse 
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy.future import select
+import asyncio
+from fastapi.concurrency import run_in_threadpool
 
 from backend.db import database
 
@@ -22,15 +26,23 @@ class CompanyBatchOutput(BaseModel):
     companies: list[CompanyOutput]
     total: int
 
-class MoveCompanyInput(BaseModel):
-    company_id: int
-    source_collection: str
-    target_collection: str
-
 class MoveMultCompaniesInput(BaseModel):
     company_ids: list[int]
     source_collection: str
     target_collection: str
+
+class MoveAllCompaniesInput(BaseModel):
+    source_collection: str
+    target_collection: str
+
+class MoveMultCompaniesOutput(BaseModel):
+    job_id: str
+    message: str
+
+class JobStatusOutput(BaseModel):
+    status: str
+    chunks_processed: int
+    chunks: int
 
 def fetch_companies_with_liked(
     db: Session, company_ids: list[int]
@@ -67,7 +79,8 @@ def fetch_companies_with_liked(
         for company, liked in results
     ]
 
-@router.get("", response_model=CompanyBatchOutput)
+
+@router.get("", response_model = CompanyBatchOutput)
 def get_companies(
     offset: int = Query(
         0, description="The number of items to skip from the beginning"
@@ -85,127 +98,163 @@ def get_companies(
         total=count,
     )
 
-@router.post("/move")
-def move_company(
-    payload:MoveCompanyInput,
-    db: Session = Depends(database.get_db),
-):
-    source_collection = (
-        db.query(database.CompanyCollection)
-        .filter(database.CompanyCollection.id == payload.source_collection)
-        .first()
-    )
-
-    if not source_collection:
-        raise HTTPException(status_code=404, detail="Source collection not found")
-
-    target_collection = (
-        db.query(database.CompanyCollection)
-        .filter(database.CompanyCollection.id == payload.target_collection)
-        .first()
-    )
-
-    if not target_collection:
-        raise HTTPException(status_code=404, detail="Target collection not found")
-
-    company_in_source = (
-        db.query(database.CompanyCollectionAssociation)
-        .filter(database.CompanyCollectionAssociation.collection_id == source_collection.id)
-        .filter(database.CompanyCollectionAssociation.company_id == payload.company_id)
-        .first()
-    )
-
-    if not company_in_source:
-        raise HTTPException(status_code=404, detail="Company not found in Source collection")
-
-    company_in_target= (
-        db.query(database.CompanyCollectionAssociation)
-        .filter(database.CompanyCollectionAssociation.collection_id == target_collection.id)
-        .filter(database.CompanyCollectionAssociation.company_id == payload.company_id)
-        .first()
-    )
-
-    if company_in_target:
-        return {"status": "exists", "message": f"Company already exists in {payload.target_collection}"}
-
-    new_entry = database.CompanyCollectionAssociation(
-        company_id = payload.company_id,
-        collection_id=target_collection.id
-    )
-
-    db.add(new_entry)
-    db.commit()
-
-    return {"status": "success", "message": f"Company moved from {payload.source_collection} to {payload.target_collection}"}
-
-def create_job(chunks: int, job_id:str) -> str:
+def create_job(job_id: str, chunks: int) -> str:
     jobs[job_id] = {
         "status": "in_progress",
         "chunks_processed": 0,
-        "chunks": chunks
+        "chunks": chunks,
     }
     return job_id
 
 def update_job_progress(job_id: str):
     if job_id in jobs:
-         jobs[job_id]["chunks_processed"] += 1
-         if jobs[job_id]["chunks_processed"] >= jobs[job_id]["chunks"]:
-             jobs[job_id]["status"] = "completed"
+        jobs[job_id]["chunks_processed"] += 1
+        if jobs[job_id]["chunks_processed"] >= jobs[job_id]["chunks"]:
+            jobs[job_id]["status"] = "completed"
 
-@router.get("/job-status/{job_id}")
-def get_job_status(job_id: str):
-    if job_id not in jobs:
-        raise HTTPException(status_code=404, detail="Job not found")
-    return jobs[job_id]
-
-def process_company_chunk(job_id: str, chunk: list, target_collection_id: int, db: Session):
-    companies_in_target =(
-        db.query(database.CompanyCollectionAssociation) 
-        .filter(database.CompanyCollectionAssociation.collection_id == target_collection_id) 
-        .filter(database.CompanyCollectionAssociation.company_id.in_(chunk)) 
-        .all()
-    ) 
-
-    existing_company_ids = set(map(lambda association: association.company_id, companies_in_target))
-    companies_to_add = list(filter(lambda company_id: company_id not in existing_company_ids, chunk))
-
-    for company_id in companies_to_add:
-        new_entry = database.CompanyCollectionAssociation(
-            company_id=company_id,
-            collection_id=target_collection_id
-        )
-        db.add(new_entry)
-
-    db.commit()
-    update_job_progress(job_id)
-
-@router.post("/moveMultiple")
-def move_mult_companies(
-    payload:MoveMultCompaniesInput,
-    background_tasks: BackgroundTasks,
-    db: Session = Depends(database.get_db),
-):
+def get_collection_by_id(collectionId: str, db: Session):
     target_collection = (
         db.query(database.CompanyCollection)
-        .filter(database.CompanyCollection.id == payload.target_collection)
+        .filter(database.CompanyCollection.id == collectionId)
         .first()
-    )
+    );
+    return target_collection
 
-    if not target_collection:
-        raise HTTPException(status_code=404, detail="Target collection not found")
+def get_companies_by_collection_id(collectionId: str, db: Session):
+    companies = (
+        db.query(database.CompanyCollectionAssociation.company_id)
+        .filter(database.CompanyCollectionAssociation.collection_id == collectionId)
+        .all()
+    );
+    return companies
 
+
+async def create_company_chunking_job(chunk_size,company_ids,target_collection,db,background_tasks):
     job_id = str(uuid.uuid4())
-
-    chunk_size = 100
-    num_companies = len(payload.company_ids)
+    chunk_size = chunk_size
+    num_companies = len(company_ids)
     chunks = math.ceil(num_companies/chunk_size)
 
     create_job(job_id, chunks)
 
     for i in range(chunks):
-        chunk = payload.company_ids[i * chunk_size : (i + 1) * chunk_size]
-        background_tasks.add_task(process_company_chunk, job_id ,chunk, target_collection.id, db)
- 
-    return {"job_id": job_id, "message": "Moving multiple companies"}
+        chunk = company_ids[i * chunk_size : (i + 1) * chunk_size]
+        background_tasks.add_task(process_company_chunk, job_id, chunk, target_collection, db)
+    return job_id
+
+async def process_company_chunk(job_id: str, chunk: list, target_collection_id: int, db: Session):
+
+    companies_in_target = db.execute(
+        select(database.CompanyCollectionAssociation.company_id)
+        .filter(database.CompanyCollectionAssociation.collection_id == target_collection_id)
+    ).scalars().all()
+
+    existing_company_ids = set(companies_in_target)
+    companies_to_add = [company_id for company_id in chunk if company_id not in existing_company_ids]
+
+    new_entries = [
+        database.CompanyCollectionAssociation(company_id=company_id, collection_id=target_collection_id)
+        for company_id in companies_to_add
+    ]
+
+    def addAndCommit(new_entries):
+        db.add_all(new_entries)
+        db.commit()
+
+    await run_in_threadpool(lambda: addAndCommit(new_entries))
+
+    update_job_progress(job_id)
+
+    return new_entries
+
+
+@router.post("/moveMultiple")
+async def move_mult_companies(
+    payload: MoveMultCompaniesInput,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(database.get_db),
+):
+    target_collection = get_collection_by_id(payload.target_collection.id,db)
+    if not target_collection:
+        raise HTTPException(status_code=404, detail="Target collection not found")
+
+    source_collection = get_collection_by_id(payload.source_collection.id,db)
+    if not source_collection:
+        raise HTTPException(status_code=404, detail="Source collection not found")
+
+    job_id = await create_company_chunking_job(100,payload.company_ids,payload.target_collection,db,background_tasks)
+
+    return MoveMultCompaniesOutput(
+        job_id = job_id,
+        message = "Move company job initiated"
+    )
+
+
+@router.post("/moveAll")
+async def move_all_companies(
+    payload: MoveAllCompaniesInput,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(database.get_db),
+):
+    target_collection = get_collection_by_id(payload.target_collection,db)
+    if not target_collection:
+        raise HTTPException(status_code=404, detail="Target collection not found")
+
+    source_collection = get_collection_by_id(payload.source_collection,db)
+    if not source_collection:
+        raise HTTPException(status_code=404, detail="Source collection not found")
+
+    companies_in_source = get_companies_by_collection_id(payload.source_collection,db)
+    if not companies_in_source:
+        raise HTTPException(status_code=404, detail="No companies found in the source collection")
+
+    company_ids = [company.company_id for company in companies_in_source]
+
+    job_id = await create_company_chunking_job(100,company_ids,payload.target_collection,db,background_tasks)
+
+    return MoveMultCompaniesOutput(
+        job_id = job_id,
+        message = "Move ALL job initiated"
+    )
+
+
+
+
+
+
+
+
+
+
+
+@router.get("/job-status/{job_id}")
+async def get_job_status(
+    job_id:str
+):
+    if job_id not in jobs:
+        raise HTTPException(status_code = 404, detail="Job not found")
+
+    async def event_stream():
+        while jobs[job_id]["status"] != "completed":    
+            progress_output = JobStatusOutput(
+                status = jobs[job_id]["status"],
+                chunks_processed = jobs[job_id]["chunks_processed"],
+                chunks = jobs[job_id]["chunks"]
+            )
+            yield f"data: {progress_output.json()}\n\n"
+            await asyncio.sleep(2)
+
+        completed_output = JobStatusOutput(
+            status = "completed",
+            chunks_processed = jobs[job_id]["chunks"],
+            chunks = jobs[job_id]["chunks"]
+        )
+        yield f"data: {completed_output.json()}\n\n"
+    
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
+
+
+
+
 
 
