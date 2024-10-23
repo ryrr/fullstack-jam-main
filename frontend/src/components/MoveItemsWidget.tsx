41ff3f6fa7c8e9eade7f544d66d66d89aaa56291
Ryan Rivera <37811096+ryrr@users.noreply.github.com>
2024-10-22 22:43:25 -0400

    refactor moveItems component
    - added some types
    - made more readable

--- frontend/src/components/MoveItemsWidget.tsx
+++ frontend/src/components/MoveItemsWidget.tsx
@@ -1,34 +1,40 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { ICollection } from "../utils/jam-api"
 
 interface MoveItemsWidgetProps {
     selectedCollectionId:string
-    collectionResponse:any
-    moveItems:(targetCollection : string) => void
+    collectionResponse:ICollection[]
+    moveItems:(targetCollectionId : string, moveType : string) => void
 }
 const MoveItemsWidget:React.FC<MoveItemsWidgetProps> = ({ selectedCollectionId, collectionResponse, moveItems })  => {
-    let collectionName = selectedCollectionId ? collectionResponse.find((collection) => collection.id === selectedCollectionId).collection_name : ''
-    let targetCollections = selectedCollectionId ? collectionResponse?.filter((collection)=>collection.id !== selectedCollectionId):[]
-    const [targetCollectionId,setTargetCollectionId] = useState<string>(targetCollections[0])
+    let selectedCollectionName : string = selectedCollectionId ? collectionResponse.find((collection) => collection.id === selectedCollectionId).collection_name : ''
+    let validTargets : ICollection[] = selectedCollectionId ? collectionResponse?.filter((collection)=>collection.id !== selectedCollectionId):[]
+    const [targetCollection,setTargetCollection] = useState<ICollection>(validTargets[0])
+    const [moveType,setMoveType] = useState<string>('SELECTED')
+
+    useEffect(() => {
+        setTargetCollection(validTargets[0])
+    }, [selectedCollectionId])
 
     return(
         <div className='flex justify-between items-center'>
             <div>
-                <button onClick={()=>{moveItems(targetCollectionId)}}>MOVE</button>
-                <select className='font-bold'>
-                    <option>SELECTED</option>
-                    <option>ALL</option>
+                <button onClick={()=>{moveItems(targetCollection.id,moveType)}}>MOVE</button>
+                <select className='font-bold' onChange={(e)=>{setMoveType(e.target.value)}}>
+                    <option value='SELECTED'>SELECTED</option>
+                    <option value='ALL'>ALL</option>
                 </select>
             </div>
             <div>
                 <span>FROM:  </span>
-                <span><b>{collectionName}</b></span>
+                <span><b>{selectedCollectionName}</b></span>
             </div>
             <div>
                 <span>TO:  </span>
-                <select className='font-bold' onChange={(e)=>{setTargetCollectionId(e.target.value)}}>
-                    {targetCollections.map((collection)=>{
+                <select className='font-bold' onChange={(e)=>{setTargetCollection(JSON.parse(e.target.value))}}>
+                    {validTargets.map((collection)=>{
                         return(
-                            <option value={collection.id}>{collection.collection_name}</option>
+                            <option value={JSON.stringify(collection)}>{collection.collection_name}</option>
                         )
                     })}
                 </select>
